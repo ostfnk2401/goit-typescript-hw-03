4918bbc9b5c720c5c9196d2b344036f0b5941a77
ostfnk2401 <117888795+ostfnk2401@users.noreply.github.com>
2023-09-24 22:28:51 +0200

    123

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,58 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-const house = new MyHouse(key);
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key | null = null;
+  protected tenants: Person[] = [];
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed. Cannot enter.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key && this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      this.key = key;
+      console.log("The door is opened.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
